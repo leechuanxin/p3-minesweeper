cb15278ba0044c1f698f39ae88a56769f61119fb
Lee Chuan Xin <chuanxin@gametize.com>
2021-09-28 07:50:30 +0800

    feat: click handlers for tiles.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const setBoardGrid = (board, rowCount, colCount) => {
   for (let i = 0; i < rowCount; i += 1) {
     const row = [];
     for (let j = 0; j < colCount; j += 1) {
-      const item = ' ';
+      const item = { value: ' ', opened: false };
       row.push(item);
     }
     emptyBoard.push(row);
@@ -38,8 +38,8 @@ const setMinesAndNumbers = (board, rowCount, colCount, mineCount) => {
     const randomRowIdx = Math.floor(Math.random() * rowCount);
     const randomColIdx = Math.floor(Math.random() * colCount);
 
-    if (dupBoard[randomRowIdx][randomColIdx] !== '*') {
-      dupBoard[randomRowIdx][randomColIdx] = '*';
+    if (dupBoard[randomRowIdx][randomColIdx].value !== '*') {
+      dupBoard[randomRowIdx][randomColIdx].value = '*';
       currMineCount += 1;
 
       for (let k = randomRowIdx - 1; k <= randomRowIdx + 1; k += 1) {
@@ -48,12 +48,12 @@ const setMinesAndNumbers = (board, rowCount, colCount, mineCount) => {
             continue;
           }
 
-          if (dupBoard[k][l] !== '*') {
-            if (dupBoard[k][l] === ' ') {
-              dupBoard[k][l] = '1';
+          if (dupBoard[k][l].value !== '*') {
+            if (dupBoard[k][l].value === ' ') {
+              dupBoard[k][l].value = '1';
             } else {
-              const number = Number(dupBoard[k][l]) + 1;
-              dupBoard[k][l] = number.toString();
+              const number = Number(dupBoard[k][l].value) + 1;
+              dupBoard[k][l].value = number.toString();
             }
           }
         }
@@ -69,7 +69,7 @@ const countMines = (board) => {
   for (let i = 0; i < board.length; i += 1) {
     const row = board[i];
     for (let j = 0; j < row.length; j += 1) {
-      if (board[i][j] === '*') {
+      if (board[i][j].value === '*') {
         sum += 1;
       }
     }
@@ -78,9 +78,30 @@ const countMines = (board) => {
   return sum;
 };
 
+const handleTileClick = (board) => (e) => {
+  const targetId = e.currentTarget.id;
+  const rowIdx = Number(targetId.split('_')[0]);
+  const colIdx = Number(targetId.split('_')[1]);
+  board[rowIdx][colIdx].opened = true;
+  if (board[rowIdx][colIdx].value.trim() === '') {
+    console.log('is empty tile');
+  } else {
+    console.log('is tile:', board[rowIdx][colIdx].value);
+  }
+  printBoard(board);
+};
+
+const handleTilesClick = (board) => {
+  const tiles = document.querySelectorAll('.col-1.unopened');
+  tiles.forEach((tile) => {
+    tile.addEventListener('click', handleTileClick(board));
+  });
+};
+
 const printBoard = (board) => {
   const minesweeper = document.querySelector('#msWrapper');
   minesweeper.classList.add('ms-wrapper');
+  minesweeper.innerHTML = '';
   // print columns
   for (let i = 0; i < board.length; i += 1) {
     const row = document.createElement('div');
@@ -89,12 +110,23 @@ const printBoard = (board) => {
       const col = document.createElement('div');
       col.className = 'col-1';
       const span = document.createElement('span');
-      span.innerText = board[i][j];
+      col.id = `${i}_${j}`;
+
+      if (!board[i][j].opened) {
+        col.classList.add('unopened');
+        span.innerText = '';
+      } else {
+        col.classList.remove('unopened');
+        span.innerText = board[i][j].value;
+      }
+
       col.appendChild(span);
       row.appendChild(col);
     }
     minesweeper.appendChild(row);
   }
+
+  handleTilesClick(board);
 };
 
 const boardWithGrid = setBoardGrid(BOARD, NUMBER_OF_ROWS, NUMBER_OF_COLS);
@@ -105,9 +137,9 @@ const boardWithMines = setMinesAndNumbers(
   MINE_COUNT,
 );
 
-console.log('starter board:', BOARD);
-console.log('empty board:', boardWithGrid);
-console.log('begin setting mines and numbers:', boardWithMines);
-console.log('total mine count:', countMines(boardWithMines));
+// console.log('starter board:', BOARD);
+// console.log('empty board:', boardWithGrid);
+// console.log('begin setting mines and numbers:', boardWithMines);
+// console.log('total mine count:', countMines(boardWithMines));
 
 printBoard(boardWithMines);
