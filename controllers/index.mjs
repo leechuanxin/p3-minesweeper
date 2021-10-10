2f79ba6fd490c54777dbc92422c6b9071c3b019e
Lee Chuan Xin <chuanxin@gametize.com>
2021-10-11 03:39:34 +0800

    feat: preliminary game cards.

--- controllers/index.mjs
+++ controllers/index.mjs
@@ -1,6 +1,14 @@
-export default function initIndexController() {
-  const index = (request, response) => {
-    response.render('index/index', { user: request.user });
+export default function initIndexController(db) {
+  const index = async (request, response) => {
+    const games = await db.Game.findAll({
+      where: {
+        isCompleted: {
+          [db.Sequelize.Op.eq]: false,
+        },
+      },
+    });
+
+    response.render('index/index', { user: request.user, games });
   };
 
   return {
