58bec1550ef9a77b3baaeffec102beddb4e16d61
Lee Chuan Xin <chuanxin@gametize.com>
2022-03-04 00:26:34 +0800

    fix: removed redundant base case.

--- controllers/games.mjs
+++ controllers/games.mjs
@@ -184,10 +184,6 @@ export default function initGamesController(db) {
       return;
     }
 
-    if (gameState.board[row][col].opened === true) {
-      return;
-    }
-
     // number or mine
     if (gameState.board[row][col].value.trim() !== '') {
       gameState.board[row][col].opened = true;
